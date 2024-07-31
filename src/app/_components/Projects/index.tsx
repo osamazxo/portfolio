e98d24ddd2c48a0f738efb1b639a2c80a57e80a3
Osama Safwat <rovendix@gmail.com>
2024-07-31 05:24:14 +0300

    finished project section in home page

--- src/app/_components/Projects/index.tsx
+++ src/app/_components/Projects/index.tsx
@@ -1,34 +1,51 @@
 import React from "react";
+import styles from "./projects.module.css";
+import eduvation from "@/assets/eduvation/cover.png";
+import tripma from "@/assets/tripma/cover.png";
+import kasper from "@/assets/kasper/cover.png";
+import wooster from "@/assets/wooster/cover.png";
+import domino from "@/assets/domino/cover.png";
+
+import Image from "next/image";
+import Link from "next/link";
+import clsx from "clsx";
 const projects = [
   {
+    id: "p10001",
     title: "Eduvation",
-    description: "",
-    img: "",
+    description:
+      "Learning hub that includes recorded courses, live workshops and focus groups",
+    img: eduvation,
   },
   {
+    id: "p10002",
     title: "Safety Sphere",
-    description: "",
+    description: "Smart Emergency Alert and Prevention System",
     img: "",
   },
   {
+    id: "p10003",
     title: "Tripma",
-    description: "",
-    img: "",
+    description: "Flight booking website built on React TS and SCSS",
+    img: tripma,
   },
   {
+    id: "p10004",
     title: "Domino",
     description: "",
-    img: "",
+    img: domino,
   },
   {
+    id: "p10005",
     title: "Kasper",
     description: "",
-    img: "",
+    img: kasper,
   },
   {
+    id: "p10006",
     title: "Wooster",
     description: "",
-    img: "",
+    img: wooster,
   },
 ];
 const Projects = () => {
@@ -36,16 +53,42 @@ const Projects = () => {
     <div id="projects" className="py-10 px-4 bg-[#faf8f8]">
       <div className="container text-center">
         <h2 className="text-3xl font-bold mb-2">Projects</h2>
-        <p className="text-lg mb-4">
+        <p className="text-lg mb-6">
           Here are some of the projects, I have worked on
         </p>
         <ul className="grid grid-cols-12 gap-4">
           {projects.map((project) => (
             <li
               key={project.title}
-              className="col-span-12 md:col-span-6 lg:col-span-4"
+              className={clsx(
+                "col-span-12 md:col-span-6 lg:col-span-4 rounded-md overflow-hidden relative",
+                styles["project-card"]
+              )}
             >
-              {project.title}
+              <Image
+                className="transition-all duration-500 "
+                src={project.img}
+                alt={project.title}
+                style={{
+                  height: "260px",
+                  objectFit: "cover",
+                }}
+              />
+              <div
+                className={clsx(
+                  "opacity-0 invisible bg-indigo-800 text-white  absolute size-full left-0 top-0 z-50 p-6  flex justify-center items-center flex-col gap-6",
+                  styles["details"]
+                )}
+              >
+                <p className="text-xl text-center font-semibold">
+                  {project.description}
+                </p>
+                <Link href={"/projects/" + project.id}>
+                  <button className="transition-all py-2 px-6 rounded-full border-4 border-indigo-500 text-lg font-bold hover:border-transparent hover:bg-indigo-500">
+                    View Project
+                  </button>
+                </Link>
+              </div>
             </li>
           ))}
         </ul>
